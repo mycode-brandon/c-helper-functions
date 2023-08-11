2d1cea35f0d5f9109888ff230ef17aebe2cc90e8
Brandon <138620228+mycode-brandon@users.noreply.github.com>
2023-08-11 08:55:50 -0500

    Update dictionary.c
    made manual version with no object constructors.

--- dictionary.c
+++ dictionary.c
@@ -6,92 +6,66 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct DictNode {
+    char *key;
+    char *value;
+    struct DictNode *next;
+};
 
 struct Dict {
-    struct DictItem *head;
-    struct DictItem *tail;
+    struct DictNode *head;
+    struct DictNode *tail;
     int length;
 };
 
-
-struct DictItem {
-    char *key;
-    char *value;
-    struct DictItem *next;
-};
-
-
 struct Dict *Dict_new() {
-    struct Dict *this_dict = malloc(sizeof(*this_dict));
+    struct Dict *this_dict = (struct Dict *) malloc(sizeof(*this_dict));
     this_dict->head = NULL;
     this_dict->tail = NULL;
     this_dict->length = 0;
-    return this_dict;
 }
-
-
-void Dict_put(struct Dict *this_dict, char *key, char *value) {
-    char *this_key = (char *) malloc(sizeof(key)+1);
-    char *this_value = (char *) malloc(sizeof(value)+1);
-    strcpy(this_key, key);
-    strcpy(this_value, value);
-
-    printf("%s\n", this_key);
-    printf("%s\n", this_value);
-
-
-    struct DictItem *new = (struct DictItem *) malloc(sizeof(struct DictItem));
-    new->key = this_key;
-    new->value = this_value;
-    new->next = NULL;
-
-    //printf("%s\n", new->key);
-    /*
-    if (this_dict->tail != NULL) {
-        this_dict->tail->next = new;
-    }
-        
-    this_dict->tail=new;
-    if (this_dict->head == NULL) {
-        this_dict->head == new;
-    }
-    */
-}
-
-
-char *Dict_get(struct Dict *this_dict, char *this_key) {
-    ;
-}
-
-
-void Dict_pop(struct Dict *this_dict, char *this_key) {
-    ;
-}
-
-void Dict_print(struct Dict *this_dict) {
-    ;
-}
-
-
-void Dict_delete(struct Dict *this_dict) {
-    struct DictItem *current, *next;
-    current = this_dict->head;
-    while(current) {
-        free(current->key);
-        free(current->value);
-        next = current->next;
-        free(current);
-        current = next;
-    }
-    free((void *) this_dict);
-}
-
 
 int main() {
+    
     struct Dict *my_dict = Dict_new();
+    
+    struct DictNode *new_node = (struct DictNode *) malloc(sizeof(*new_node));
 
-    Dict_put(my_dict, "a", "b");
-
-    Dict_delete(my_dict);
+    char keys[5][100] = {
+        "key1",
+        "key2",
+        "key3",
+        "key4",
+        "key5"
+    };
+    
+    char values[5][100] = {
+        "val1",
+        "val2",
+        "val3",
+        "val4",
+        "val5"
+    };
+    
+    new_node->key = keys[0];
+    new_node->value = values[0];
+    
+    my_dict->head = new_node;
+    my_dict->tail = new_node;
+    
+    struct DictNode *new_node2 = (struct DictNode *) malloc(sizeof(*new_node));
+    new_node2->key = keys[1];
+    new_node2->value = values[1];
+    my_dict->tail->next = new_node2;
+    my_dict->tail = new_node2;
+    
+    printf("my_dict->head = %p\n", my_dict->head);
+    printf("my_dict->tail = %p\n", my_dict->tail);
+    for (struct DictNode *current = my_dict->head; current != NULL; current = current->next) {
+        printf("%s\n", current->key);
+        printf("%s\n", current->value);
+        printf("%p\n", current);
+    }
+    
     return 0;
 }
