3005750514296b890d3e575467588e55de0cecf6
Brandon <138620228+mycode-brandon@users.noreply.github.com>
2023-08-18 12:52:54 -0500

    Update hashMap.c
    Completed put method and dump method.

--- hashMap.c
+++ hashMap.c
@@ -52,7 +52,46 @@ int __HashMap_hash(char *key) {
 
 
 void __HashMap_put(struct HashMap *this_hashmap, char *key, int value) {
-    int bucket = __HashMap_hash(key);
+    //check if key exists
+    int key_exists = 0;
+    for (int i = 0; i < MAX_BUCKETS; ++i) {
+        for (struct BucketNode *current = this_hashmap->__heads[i]; current != NULL; current = current->__next) {
+            if (strcmp(key, current->key) == 0) {
+                key_exists = 1;
+            }
+        }
+    }
+    //create new bucket node if key doesn't already exist
+    if (key_exists == 0) {
+        int bucket = __HashMap_hash(key);
+        char *this_key = malloc(sizeof(key));
+        strcpy(this_key, key);
+        struct BucketNode *new_BucketNode = (struct BucketNode *) malloc(sizeof(*new_BucketNode));
+        
+        new_BucketNode->__prev = this_hashmap->__tails[bucket];
+        new_BucketNode->__next = NULL;
+        new_BucketNode->key = this_key;
+        new_BucketNode->value = value;
+        this_hashmap->__tails[bucket]->__next = new_BucketNode;
+        this_hashmap->__tails[bucket] = new_BucketNode;
+        
+        //only first node in each bucket
+        if (this_hashmap->__heads[bucket] == NULL) {
+            this_hashmap->__heads[bucket] = new_BucketNode;
+        }
+    }
+    
+}
+
+
+void __HashMap_dump(struct HashMap *this_hashmap) {
+    printf("[");
+    for (int i = 0; i < MAX_BUCKETS; i++) {
+        for (struct BucketNode *current = this_hashmap->__heads[i]; current != NULL; current = current->__next) {
+            printf("'%s' : %d, ", current->key, current->value);
+        }
+    }
+    printf("]");
 }
 
 
@@ -120,6 +159,9 @@ int main()
     
     struct HashMap *my_hashmap = HashMap_new();
     my_hashmap->put(my_hashmap, "key1", 1);
+    my_hashmap->put(my_hashmap, "key1", 2);
+    my_hashmap->put(my_hashmap, "key2", 42);
+    my_hashmap->put(my_hashmap, "key3", 54);
 
     return 0;
 }
